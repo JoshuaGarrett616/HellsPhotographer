4425f3c85d91af4daf2e36e53029426a0ba9b08a
JoshuaGarrett616 <43422786+JoshuaGarrett616@users.noreply.github.com>
2020-08-14 20:09:00 -0500

    Initial Commit

---
+++ Source/HellsPhotographer/HellsPhotographerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HellsPhotographerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLSPHOTOGRAPHER_API AHellsPhotographerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
